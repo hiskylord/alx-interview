3feced232f05baa11dfc4d57d2f87746b6bbadcf
mac <mac@macs-MacBook-Air.local>
2022-12-23 05:09:29 +0100

    ps

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,20 +7,15 @@ def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
     '''
-    triangle = []
+    pt = []
     if type(n) is not int or n <= 0:
-        return triangle
-    for i in range(n):
-        line = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                line.append(1)
-            elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
-    return triangle
-
-
-# jrjr r
-
-# print(pascal_triangle(2))
+        return pt
+    for x in range(n):  # 1
+        ct = []
+        for y in range(x+1):
+            if y == 0 or y == x:
+                ct.append(1)
+            else:
+                ct.append(pt[x-1][y]+pt[x-1][y-1])
+        pt.append(ct)
+    return pt
